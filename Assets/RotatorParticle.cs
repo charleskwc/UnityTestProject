07a27afb4a8dce552b0ecc4d76803985e966ea89
Richard Kong <richard.kong@agworld.com.au>
2014-01-19 15:21:49 +0800

    More prefabs
    Rotating particles

---
+++ Assets/RotatorParticle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatorParticle : MonoBehaviour {
+
+	public float rotationSpeedParticle;
+	public Vector3 originalPositionParticle; // point A
+
+	// Use this for initialization
+	void Start () {
+		originalPositionParticle = transform.position;
+		transform.Translate (1, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(originalPositionParticle, Vector3.up, rotationSpeedParticle * Time.deltaTime);
+	}
+}
--- Assets/scripts/Rotator.cs
+++ Assets/scripts/Rotator.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour
 {
+	public float rotationSpeed;
+	public Vector3 originalPosition; // point A
 
-	// Update is called once per frame
+	void Start (){
+		//rotationSpeed = 45;
+		originalPosition = transform.position;
+		transform.Translate (1, 0, 0);
+	}
+
 	void Update () {
+		//Vector3 axis = B - A;// calculate axis
 		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+		transform.RotateAround(originalPosition, Vector3.up, rotationSpeed * Time.deltaTime);
 	}
 }
